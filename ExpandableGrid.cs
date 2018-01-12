8d98a9d49ea0b45f807082af9f9319e370627e40
MahonHughes <34442699+MahonHughes@users.noreply.github.com>
2018-01-12 12:52:11 +0000

    Create ExpandableGrid.cs

---
+++ ExpandableGrid.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_of_increace
+{
+    class Program
+    {
+
+        static void BuildAll()
+        {
+            int size = 0;
+            string input = Console.ReadLine();
+            size = Convert.ToInt32(input);
+            if (size == 0)
+            {
+                Console.WriteLine("Excuse me?");
+                System.Threading.Thread.Sleep(3000);
+                Console.WriteLine("Could you please leave?");
+                System.Threading.Thread.Sleep(5000);
+                //Console.Read();
+                size = 88888;
+            }
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            string pop = " ";
+            Console.Clear();
+
+            char letter = 'A';
+            Console.Write("    ");
+            for (var y = 0; y < size; y++)
+            {
+                Console.Write(letter);
+                Console.Write("   ");
+                letter++;
+            }
+            Console.WriteLine("");
+            Console.Write("  ┏");
+            for (int a = 0; a < (size - 1); a++)
+            {
+                Console.Write("━━━");
+                Console.Write("┳");
+            }
+            Console.Write("━━━┓");
+
+            for (int b = 0; b < (size - 1); b++)
+            {
+                Console.WriteLine("");
+
+                if (b < 9)
+                {
+                    Console.Write(b + 1);
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.Write(b + 1);
+                }
+
+                for (int c = 0; c < size; c++)
+                {
+                    Console.Write("┃");
+                    Console.Write(" " + pop + " ");
+                }
+
+                Console.Write("┃");
+                Console.WriteLine("");
+                Console.Write("  ┣");
+                for (int a = 0; a < (size - 1); a++)
+                {
+                    Console.Write("━━━");
+                    Console.Write("╋");
+                }
+                Console.Write("━━━┫");
+            }
+            Console.WriteLine("");
+            if (size <= 9)
+            {
+                Console.Write(size);
+                Console.Write(" ");
+            }
+            else
+            {
+                Console.Write(size);
+            }
+
+            for (int c = 0; c < size; c++)
+            {
+                Console.Write("┃");
+                Console.Write(" " + pop + " ");
+            }
+            Console.Write("┃");
+            Console.WriteLine("  ");
+            Console.Write("  ┗");
+            for (int a = 0; a < (size - 1); a++)
+            {
+                Console.Write("━━━");
+                Console.Write("┻");
+            }
+            Console.Write("━━━┛");
+
+            // ┗━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┛
+
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                BuildAll();
+            }
+
+        }
+    }
+}
