243301b3558d5a6d6a7e1596af6aee8b6e6e596c
MahonHughes <34442699+MahonHughes@users.noreply.github.com>
2018-01-15 16:30:34 +0000

    Update ExpandableGrid.cs

--- ExpandableGrid.cs
+++ ExpandableGrid.cs
@@ -6,112 +6,112 @@ using System.Threading.Tasks;
 
 namespace Test_of_increace
 {
-    class Program
-    {
+    class Program
+    {
 
-        static void BuildAll()
-        {
-            int size = 0;
-            string input = Console.ReadLine();
-            size = Convert.ToInt32(input);
-            if (size == 0)
-            {
-                Console.WriteLine("Excuse me?");
-                System.Threading.Thread.Sleep(3000);
-                Console.WriteLine("Could you please leave?");
-                System.Threading.Thread.Sleep(5000);
+        static void BuildAll()
+        {
+            int size = 0;
+            string input = Console.ReadLine();
+            size = Convert.ToInt32(input);
+            if (size == 0)
+            {
+                Console.WriteLine("Excuse me?");
+                System.Threading.Thread.Sleep(3000);
+                Console.WriteLine("Could you please leave?");
+                System.Threading.Thread.Sleep(5000);
                 //Console.Read();
                 size = 88888;
-            }
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
-            string pop = " ";
-            Console.Clear();
+            }
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            string pop = " ";
+            Console.Clear();
 
-            char letter = 'A';
-            Console.Write("    ");
-            for (var y = 0; y < size; y++)
-            {
-                Console.Write(letter);
-                Console.Write("   ");
-                letter++;
-            }
-            Console.WriteLine("");
-            Console.Write("  ┏");
-            for (int a = 0; a < (size - 1); a++)
-            {
-                Console.Write("━━━");
-                Console.Write("┳");
-            }
-            Console.Write("━━━┓");
+            char letter = 'A';
+            Console.Write("    ");
+            for (var y = 0; y < size; y++)
+            {
+                Console.Write(letter);
+                Console.Write("   ");
+                letter++;
+            }
+            Console.WriteLine("");
+            Console.Write("  ┏");
+            for (int a = 0; a < (size - 1); a++)
+            {
+                Console.Write("━━━");
+                Console.Write("┳");
+            }
+            Console.Write("━━━┓");
 
-            for (int b = 0; b < (size - 1); b++)
-            {
-                Console.WriteLine("");
+            for (int b = 0; b < (size - 1); b++)
+            {
+                Console.WriteLine("");
 
-                if (b < 9)
-                {
-                    Console.Write(b + 1);
-                    Console.Write(" ");
-                }
-                else
-                {
-                    Console.Write(b + 1);
-                }
+                if (b < 9)
+                {
+                    Console.Write(b + 1);
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.Write(b + 1);
+                }
 
-                for (int c = 0; c < size; c++)
-                {
-                    Console.Write("┃");
-                    Console.Write(" " + pop + " ");
-                }
+                for (int c = 0; c < size; c++)
+                {
+                    Console.Write("┃");
+                    Console.Write(" " + pop + " ");
+                }
 
-                Console.Write("┃");
-                Console.WriteLine("");
-                Console.Write("  ┣");
-                for (int a = 0; a < (size - 1); a++)
-                {
-                    Console.Write("━━━");
-                    Console.Write("╋");
-                }
-                Console.Write("━━━┫");
-            }
-            Console.WriteLine("");
-            if (size <= 9)
-            {
-                Console.Write(size);
-                Console.Write(" ");
-            }
-            else
-            {
-                Console.Write(size);
-            }
+                Console.Write("┃");
+                Console.WriteLine("");
+                Console.Write("  ┣");
+                for (int a = 0; a < (size - 1); a++)
+                {
+                    Console.Write("━━━");
+                    Console.Write("╋");
+                }
+                Console.Write("━━━┫");
+            }
+            Console.WriteLine("");
+            if (size <= 9)
+            {
+                Console.Write(size);
+                Console.Write(" ");
+            }
+            else
+            {
+                Console.Write(size);
+            }
 
-            for (int c = 0; c < size; c++)
-            {
-                Console.Write("┃");
-                Console.Write(" " + pop + " ");
-            }
-            Console.Write("┃");
-            Console.WriteLine("  ");
-            Console.Write("  ┗");
-            for (int a = 0; a < (size - 1); a++)
-            {
-                Console.Write("━━━");
-                Console.Write("┻");
-            }
-            Console.Write("━━━┛");
+            for (int c = 0; c < size; c++)
+            {
+                Console.Write("┃");
+                Console.Write(" " + pop + " ");
+            }
+            Console.Write("┃");
+            Console.WriteLine("  ");
+            Console.Write("  ┗");
+            for (int a = 0; a < (size - 1); a++)
+            {
+                Console.Write("━━━");
+                Console.Write("┻");
+            }
+            Console.Write("━━━┛");
 
             // ┗━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┻━━━┛
 
-            Console.ReadLine();
-        }
-        static void Main(string[] args)
-        {
+            //Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
 
-            while (true)
-            {
-                BuildAll();
-            }
+            while (true)
+            {
+                BuildAll();
+            }
 
-        }
-    }
+        }
+    }
 }
